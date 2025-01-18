94714aa62c7cb28969f38a261fc649513b3644fb
Andrews Goes <andrews.goes.33@gmail.com>
2025-01-17 21:36:21 -0300

    dados salvos no postgres pelo render e validados pelo PGAdmin

--- src/database.py
+++ src/database.py
@@ -1,17 +1,17 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Float, String, Integer, DateTime
+from sqlalchemy import Column, Float, Integer, String, DateTime
 from datetime import datetime
 
-#Cria a classe Base do SQLAlchemy (na versão 2.x)
-
+#Cria a classe Base para definir a tipagem dos dados da tabela do SQLALCHEMY (na versão 2.x)
 Base = declarative_base()
 
 class BitcoinPreco(Base):
-    """Define a tabela no banco de dados."""
-    __tablename__ = "bitcoin_precos"
     
+    __tablename__ = "bitcoin_preco"
+    """Definindo a tipagem das colunas"""
     id = Column(Integer, primary_key=True, autoincrement=True)
     valor = Column(Float, nullable=False)
-    criptomoeda = Column(String(50), nullable=False) #até 50 caracteres
-    moeda = Column(String(10), nullable=False) #até 10 caracteres
+    criptomoeda = Column(String, nullable=False)
+    moeda = Column(String, nullable=False)
     timestamp = Column(DateTime, default=datetime.now)
+
--- src/pipeline_05.py
+++ src/pipeline_05.py
@@ -12,5 +12,81 @@ from database import Base, BitcoinPreco
 #Carrega variáveis de ambiente do arquivo .env
 load_dotenv()
 
+#lê as variáveis separadas do arquivo .env (sem SSL)
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
+#Monta a URL de conexão ao banco POSTGRESQL (sem ?sslmode=...)
+DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
+)
 
+#Cria o engine e a sessão
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+def criar_tabela():
+    """Cria a tabela no banco de dados, se não existir."""
+    Base.metadata.create_all(engine)
+    print("Tabela criada/verificada com sucesso!")
+    
+    
+def extrair_dados_bitcon():
+    """Extrai JSON completo da API da CoinBase"""
+    url = 'https://api.coinbase.com/v2/prices/spot'
+    resposta = requests.get(url)
+    if resposta.status_code == 200:
+        return resposta.json()
+    else:
+        print(f"Erro na API: {resposta.status_code}")
+        return None
+    
+def tratar_dados_bitcoin(dados_json):
+    """Transforma os dados brutos da API e adiciona timestamp"""
+    valor = float(dados_json['data']['amount'])
+    criptomoeda = dados_json['data']['base']
+    moeda = dados_json['data']['currency']
+    timestamp = datetime.now()
+    
+    dados_tratados = {
+        "valor" : valor,
+        "criptomoeda" : criptomoeda,
+        "moeda" : moeda,
+        "timestamp" : timestamp
+        
+    }
+    
+    return dados_tratados
+
+def salvar_dados_postgres(dados):
+    """Salva os dados no banco PostgreSQL."""
+    session = Session()
+    novo_registro = BitcoinPreco(**dados)
+    session.add(novo_registro)
+    session.commit()
+    session.close()
+    print(f"[{dados['timestamp']}] Dados salvos no PostgreSQL!")
+    
+if __name__ == "__main__":
+    criar_tabela()
+    print("Iniciando ETL com atuaização a cada 15 segundos... (CTRL+C para interromper)")
+    
+    while True:
+        try:
+            dados_json = extrair_dados_bitcon()
+            if dados_json:
+                dados_tratados = tratar_dados_bitcoin(dados_json)
+                print("Dados Tratados:", dados_tratados)
+                salvar_dados_postgres(dados_tratados)
+            time.sleep(15)
+        except KeyboardInterrupt:
+            print("\nProcesso interrompido pelo usuário. Finalizando...")
+            break
+        except Exception as e:
+            print(f"Erro durante a execução: {e}")
+            time.sleep(15)
